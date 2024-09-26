f69d19020795f14c204641dc1b85abdfecb3a05c
Sadiq Salau <sadiqsalau888@gmail.com>
2024-09-26 14:31:29 +0100

    [UPDATE] Portal Major Fullscreen Spinner

--- src/drops/major/components/MajorFullscreenSpinner.jsx
+++ src/drops/major/components/MajorFullscreenSpinner.jsx
@@ -1,11 +1,13 @@
+import { createPortal } from "react-dom";
 import MajorIcon from "../assets/images/icon.png?format=webp";
 
 export default function MajorFullscreenSpinner() {
-  return (
-    <div className="absolute inset-0 flex items-center justify-center bg-black/50">
+  return createPortal(
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50">
       <div className="p-4 bg-white rounded-full">
         <img src={MajorIcon} className="w-10 h-10 rounded-full animate-spin" />
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
